290067e47038327dd18de3e07e0412d87a7d5fdb
fbitcpc <fbitc.pc@gmail.com>
2025-07-19 21:17:47 +0300

    remove files

--- tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-    temp_list = list()
-        while len(temp_list) <= quantity-1:
-            _random = randrange(min,max)
-            if  _random not in temp_list:
-                temp_list.append(_random)
-        temp_list.sort()
